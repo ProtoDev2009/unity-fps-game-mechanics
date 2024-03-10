32f799e355b00c8df8fc373c22f56796212da617
ProtoDevStuff <prototypedeveloper2009@gmail.com>
2024-03-10 21:00:56 +0530

    Basic Jump Functionality

--- FPS Game Mechanics/Assets/Scripts/Player/PlayerMovement.cs
+++ FPS Game Mechanics/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool canJump = true;
+
+    public KeyCode jumpKey = KeyCode.Space;
+
     public float playerHeight;
     public float groundDrag;
     public LayerMask groundMask;
@@ -38,6 +45,14 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         VerticalInput = Input.GetAxisRaw("Vertical");
 
+        //jump Input
+        if(Input.GetKey(jumpKey) && canJump && isGrounded)
+        {
+            canJump = false;
+            Jump();
+            Invoke(nameof(resetJump), jumpCooldown);
+        }
+
         if (isGrounded) rb.drag = groundDrag;
         else rb.drag = 0;
     }
@@ -52,7 +67,8 @@ public class PlayerMovement : MonoBehaviour
         //calculate movement direction
         moveDirection = orientation.forward * VerticalInput + orientation.right * horizontalInput;
 
-        rb.AddForce(moveDirection.normalized *  moveSpeed * 10f, ForceMode.Force);
+        if(isGrounded) rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        else if(!isGrounded) rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
     }
 
     private void SpeedControl()
@@ -62,7 +78,18 @@ public class PlayerMovement : MonoBehaviour
         if(flatVelo.magnitude > moveSpeed)
         {
             Vector3 limitLevel = flatVelo.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitLevel.x, 0f, limitLevel.z);
+            rb.velocity = new Vector3(limitLevel.x, rb.velocity.y, limitLevel.z);
         }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void resetJump()
+    {
+        canJump = true;
     }
 }
