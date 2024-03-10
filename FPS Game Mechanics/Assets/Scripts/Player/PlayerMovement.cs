cbd3c5dc51e66eab3e133cdfa9df4b39c0aad530
ProtoDevStuff <prototypedeveloper2009@gmail.com>
2024-03-10 20:40:53 +0530

    Drag And Speed Control

--- FPS Game Mechanics/Assets/Scripts/Player/PlayerMovement.cs
+++ FPS Game Mechanics/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float playerHeight;
+    public float groundDrag;
+    public LayerMask groundMask;
+    bool isGrounded;
+
     public float moveSpeed;
 
     public Transform orientation;
@@ -24,8 +30,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //check if grounded
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 2f, groundMask);
+
+        SpeedControl();
+
         horizontalInput = Input.GetAxisRaw("Horizontal");
         VerticalInput = Input.GetAxisRaw("Vertical");
+
+        if (isGrounded) rb.drag = groundDrag;
+        else rb.drag = 0;
     }
 
     private void FixedUpdate()
@@ -39,5 +53,16 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = orientation.forward * VerticalInput + orientation.right * horizontalInput;
 
         rb.AddForce(moveDirection.normalized *  moveSpeed * 10f, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVelo = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVelo.magnitude > moveSpeed)
+        {
+            Vector3 limitLevel = flatVelo.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitLevel.x, 0f, limitLevel.z);
+        }
     }
 }
